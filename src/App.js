b7922aa0e2cee3a18cbd7e8871ee847fb0a6d97d
Cong Hoang <18hcong@gmail.com>
2023-03-01 02:05:17 +0700

    Nested Routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
-import { Link } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app-container">
-      <Header />
-      <div>test link</div>
-      <div>
-        <button>
-          <Link to="/admins">Press to Admin Page</Link>
-        </button>
-        <button>
-          <Link to="/users">Press to User Page</Link>
-        </button>
+      <div className="header-container">
+        <Header />
       </div>
+      <div className="main-container">
+        <div className="sidebar-container"></div>
+      </div>
+      <div className="app-content"></div>
+      <Outlet />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -15,12 +15,13 @@ root.render(
     {/* <React.StrictMode> */}
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<App/>}/>
-      <Route path="admins" element={<Admin/>}/>
-      <Route path="users" element={<User/>}/>
+        <Route path="/" element={<App />}>
+          <Route path="admins" element={<Admin />} />
+          <Route path="users" element={<User />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-    {/* </React.StrictMode> */} 
+    {/* </React.StrictMode> */}
   </Provider>
 );
 
