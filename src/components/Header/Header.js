e7680e415b1ca15bcc917a5f5cfe8592064d22df
Cong Hoang <18hcong@gmail.com>
2023-03-01 08:42:12 +0700

    index router

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,10 @@ const Header = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">CongHoang Dev</Navbar.Brand>
+        {/* <Navbar.Brand href="#home">CongHoang Dev</Navbar.Brand> */}
+        <NavLink to="/" className={"navbar-brand"}>
+          CongHoang Dev
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -28,9 +31,9 @@ const Header = () => {
           </Nav>
           <Nav>
             <NavDropdown title="Setting" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Log In</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Log Out</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+              <NavDropdown.Item>Log In</NavDropdown.Item>
+              <NavDropdown.Item>Log Out</NavDropdown.Item>
+              <NavDropdown.Item>Profile</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,7 @@
+const HomePage = (props) => {
+  return (
+   <div className="homepage-container">Home Page</div>
+  )
+};
+
+export default HomePage;
