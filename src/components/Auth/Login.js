8d6f2ac13cd1a410cf6b39805baf2504dd039af9
Hoang Cong <18hcong@gmail.com>
2023-03-22 03:01:45 +0700

    api login

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -2,17 +2,34 @@ import { divide } from 'lodash';
 import { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import './Login.scss';
+import { useNavigate } from 'react-router-dom';
+import { postLogin } from '../../services/apiServices';
+import { toast } from 'react-toastify';
 
 const Login = (props) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const handleLogin = () => {
-    // props.handleLogin(email, password);
-    alert('Login successful')
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    //validate
+
+    //submit apis
+    let data = await postLogin(email, password);
+    if (data && data.EC === 0) {
+      toast.success(data.EM);
+      navigate('/');
+    }
+    if (data && +data.EC !== 0) {
+      toast.error(data.EM);
+    }
   };
   return (
     <div className="login-container">
-      <div className="header">Don't have an account yet?</div>
+      <div className="header">
+        Don't have an account yet?
+        <button> Sign Up</button>
+      </div>
       <div className="title col-3 mx-auto">CongHoangdev</div>
       <div className="welcome col-3 mx-auto">Hello, who's this company</div>
       <div className="content-form col-3 mx-auto">
@@ -38,9 +55,19 @@ const Login = (props) => {
         </div>
         <span className="forgot-password">Forgot password?</span>
         <div>
-          <button className="btn btn-primary" onClick={()=>handleLogin()}>
+          <button className="btn btn-primary" onClick={() => handleLogin()}>
             Login to CHdeveloper
-            </button>
+          </button>
+          <div className="text-center">
+            <span
+              className="back"
+              onClick={() => {
+                navigate('/');
+              }}
+            >
+              &#60;&#60; Go to Homepage
+            </span>
+          </div>
         </div>
       </div>
     </div>
