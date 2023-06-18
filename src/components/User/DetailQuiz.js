f01b84bae962984d0962a68ffbf9b11f84b245ec
Cong Hoang <63083089+18hcong@users.noreply.github.com>
2023-06-18 12:09:18 +0700

    countdown timer

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -168,7 +168,9 @@ const DetailQuiz = (props) => {
       </div>
       <div className="right-content">
         <RightContent
-        dataQuiz={dataQuiz}/>
+        dataQuiz={dataQuiz}
+        handleFinishQuiz={handleFinishQuiz}
+        />
       </div>
       <ModalResult
         show={isShowModalResult}
